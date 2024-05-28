824cb4544eb2715376fc5b32a8c182fd1b2506c0
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2024-05-28 12:05:55 +0700

    edit ParentComponent and ChildComponent using destructuring props

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import ParentComponent from "./components/ParentComponent";
 
 const App = () => {
 	return (
-		<div className="container mt-12 mx-auto p-8">
-			<h1 className="text-3xl font-bold mb-8">My React App</h1>
+		<div className="container p-8 mx-auto mt-12">
+			<h1 className="mb-8 text-3xl font-bold">My React App</h1>
 			<ParentComponent />
 		</div>
 	);
--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ChildComponent = (props) => {
-	return (
-		<div className="p-4 mb-4 font-semibold bg-orange-300 rounded-lg shadow-md text-md">
-			<h2 className="mb-2 text-xl font-bold">Child Component</h2>
-			<p className="text-lg text-gray-700">Name: {props.name}</p>
-			<p className="text-lg text-gray-700">Age: {props.age}</p>
-			<p className="text-lg text-gray-700">Location: {props.location}</p>
-		</div>
-	);
-};
-
-//// to set a default prop is by destructuring the props object
-// const ChildComponent = ({ name, age, location = "No Location" }) => {
+// const ChildComponent = (props) => {
 // 	return (
 // 		<div className="p-4 mb-4 font-semibold bg-orange-300 rounded-lg shadow-md text-md">
-// 			<h2 className="mb-2 text-lg font-bold">Child Component</h2>
-// 			<p className="text-gray-700">Name: {name}</p>
-// 			<p className="text-gray-700">Age: {age}</p>
-// 			<p className="text-gray-700">Location: {location}</p>
+// 			<h2 className="mb-2 text-xl font-bold">Child Component</h2>
+// 			<p className="text-lg text-gray-700">Name: {props.name}</p>
+// 			<p className="text-lg text-gray-700">Age: {props.age}</p>
+// 			<p className="text-lg text-gray-700">Location: {props.location}</p>
 // 		</div>
 // 	);
 // };
+
+//// to set a default prop is by destructuring the props object
+const ChildComponent = ({ name, age, location = "No Location" }) => {
+	return (
+		<div className="p-4 mb-4 font-semibold bg-orange-300 rounded-lg shadow-md text-md">
+			<h2 className="mb-2 text-lg font-bold">Child Component</h2>
+			<p className="text-gray-700">Name: {name}</p>
+			<p className="text-gray-700">Age: {age}</p>
+			<p className="text-gray-700">Location: {location}</p>
+		</div>
+	);
+};
 
 ChildComponent.propTypes = {
 	name: PropTypes.string.isRequired,
